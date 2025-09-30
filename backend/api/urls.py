d983c9f753899835c80460ef2db353a05c05d1ec
Altynbek Adilkhan <adilkhanaltynbek@MBPA.local>
2025-09-30 16:13:11 +0500

    Dev | server-side | add serializer

---
+++ backend/api/urls.py
@@ -0,0 +1,16 @@
+# backend/api/urls.py
+
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+# from .views import ItemViewSet, ollama_message, openai_message
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet)
+
+urlpatterns = [
+    # path('chat/ollama/', ollama_message, name='ollama_message'),
+    # path('chat/openai/', openai_message, name='openai_message'),
+]
+
+# Добавляем маршруты из роутера (ItemViewSet)
+urlpatterns += router.urls
