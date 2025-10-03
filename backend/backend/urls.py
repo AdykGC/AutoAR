3ce21383d24f0a39dd51f0573d70365745898008
Altynbek Adilkhan <adilkhanaltynbek@MBPA.local>
2025-10-03 20:04:25 +0500

    Dev | server-side | create test model for API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls), # python manage.py createsuperuser   |   A A@gmail.com A
 
 
     path('api/', include('api.urls')),   # подключаем маршруты приложения api
