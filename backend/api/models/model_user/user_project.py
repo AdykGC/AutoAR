4cf7199116792de65b2036496a6ad012483e3c55
Lucky201201021 <119715895+Lucky201201021@users.noreply.github.com>
2025-10-15 20:39:14 +0500

    Added Company model

--- backend/api/models/model_user/user_project.py
+++ backend/api/models/model_user/user_project.py
@@ -24,3 +24,16 @@ class User_Project(models.Model):
     @property
     def likes_count(self):
         return self.liked_by.count()
+    
+class Company(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_companies')
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    employees = models.ManyToManyField(User, related_name='companies', blank=True)
+
+    def __str__(self):
+        return f"Company '{self.name}' (owner: {self.owner.username})"
+
+    @property
+    def employees_count(self):
+        return self.employees.count()
